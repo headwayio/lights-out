dc93bbdfa9e52a426322112edb39e6625214dcf6
Jesse Hixson <hixsonj@gmail.com>
2022-11-18 15:20:11 -0500

    Wire up template click event to LiveView module

--- lib/lights_out_game_web/live/board.ex
+++ lib/lights_out_game_web/live/board.ex
@@ -3,7 +3,16 @@ defmodule LightsOutGameWeb.Board do
 
   def mount(_params, _session, socket) do
     grid = for x <- 0..4, y <- 0..4, into: %{}, do: {{x, y}, false}
-    # %{{0, 0} => false, {0, 1} => false, ...}
     {:ok, assign(socket, grid: grid)}
+  end
+
+  def handle_event("toggle", %{"x" => strX, "y" => strY}, socket) do
+    grid = socket.assigns.grid
+    grid_x = String.to_integer(strX)
+    grid_y = String.to_integer(strY)
+
+    updated_grid = Map.put(grid, {grid_x, grid_y}, !grid[{grid_x, grid_y}])
+
+    {:noreply, assign(socket, :grid, updated_grid)}
   end
 end
