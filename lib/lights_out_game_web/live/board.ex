561c8bfe9cdac1693ee03ab81a513bdf7b6f4a1e
Jesse Hixson <hixsonj@gmail.com>
2022-11-23 14:16:35 -0500

    Update toggle to reflect game logic

--- lib/lights_out_game_web/live/board.ex
+++ lib/lights_out_game_web/live/board.ex
@@ -3,6 +3,8 @@ defmodule LightsOutGameWeb.Board do
 
   def mount(_params, _session, socket) do
     grid = for x <- 0..4, y <- 0..4, into: %{}, do: {{x, y}, false}
+    level1 = %{{2, 0} => true, {2, 2} => true, {2, 4} => true}
+    grid = Map.merge(grid, level1)
     {:ok, assign(socket, grid: grid)}
   end
 
@@ -11,8 +13,22 @@ defmodule LightsOutGameWeb.Board do
     grid_x = String.to_integer(strX)
     grid_y = String.to_integer(strY)
 
-    updated_grid = Map.put(grid, {grid_x, grid_y}, !grid[{grid_x, grid_y}])
+    updated_grid =
+      find_adjacent_tiles(grid_x, grid_y)
+      |> Enum.reduce(%{}, fn point, acc ->
+        Map.put(acc, point, !grid[point])
+      end)
+      |> then(fn toggled_grid -> Map.merge(grid, toggled_grid) end)
 
     {:noreply, assign(socket, :grid, updated_grid)}
+  end
+
+  defp find_adjacent_tiles(x, y) do
+    prevX = Kernel.max(0, x - 1)
+    nextX = Kernel.min(4, x + 1)
+    prevY = Kernel.max(0, y - 1)
+    nextY = Kernel.min(4, y + 1)
+
+    [{x, y}, {prevX, y}, {nextX, y}, {x, prevY}, {x, nextY}]
   end
 end
